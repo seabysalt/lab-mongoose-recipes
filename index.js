b142fd31f48a3d376d01f4f817e1e2dcbae9ce21
seabysalt <seabysalt@gmail.com>
2019-06-28 12:09:46 +0200

    done

--- index.js
+++ index.js
@@ -10,3 +10,49 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+let iteration1 = Recipe.create({ 
+  title: 'Whoopies',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup brown sugar', '2 large egg', '2 tablespoons cream', '1 1/4 teaspoons vanilla extract', '3 marshmallows'],
+  cuisine: 'American',
+  dishType: 'Snack',
+  image: 'https://d2gk7xgygi98cy.cloudfront.net/1058-3-large.jpg',
+  duration: 30,
+  creator: 'Sarah Alt'
+})
+  .then(data => {
+    console.log("Whoopies");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+let iteration2 = Recipe.insertMany(data).then(d => {
+  d.forEach(x => console.log(x.title))
+})
+.catch(err => {
+  console.log(err);
+});
+
+let iteration3 = Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  .then(data => {
+    console.log("You made it! Rigatoni has a new duration!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+let iteration4 = Recipe.deleteOne({title: 'Carrot Cake'})
+  .then(data => {
+    console.log("Yay! Carrot Cake is deleted!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Promise.all([iteration1, iteration2, iteration3, iteration4])
+  .then(() => {
+    mongoose.connection.close()
+});
+
+
